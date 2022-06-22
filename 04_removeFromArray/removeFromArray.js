f97cd91cc72ed008e06b98a375828fb34bd0ec06
Adrien <adrien.biz@gmail.com>
2022-06-22 22:41:37 +0400

    Improve removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function(arr, remove) {
-    const start = arr.indexOf(remove)
-    arr.splice(start, 1);
+const removeFromArray = function(arr, value1, value2, value3, value4) {
+    for (let i = 1; i < arguments.length; i++){
+        if (arr.indexOf(arguments[i]) !== -1) {
+            const start = arr.indexOf(arguments[i]);
+            arr.splice(start, 1);
+        }
+    }
     return arr;
 };
 

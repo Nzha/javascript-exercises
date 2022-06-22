24bd6789b848fc0cbff58c75a4c373828bc77856
Adrien <adrien.biz@gmail.com>
2022-06-22 21:32:53 +0400

    Create valid function to reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    arr = str.split("");
+    reverseArr = arr.reverse();
+    return reverseArr.join("");
 };
 
 // Do not edit below this line

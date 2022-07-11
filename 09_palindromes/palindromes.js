336a5440010a40e8d676b2524b34d4f541029b5b
Adrien <adrien.biz@gmail.com>
2022-07-11 14:38:05 +0400

    Create palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    strStrip = str.replace(/!|\.| |,/g, '').toLowerCase();
+    reverseStrStrip = strStrip.split('').reverse().join('');
+    if (reverseStrStrip === strStrip) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line

d596ee29a584221cba37035ee3feca4d3fe00e12
Adrien <adrien.biz@gmail.com>
2022-07-12 22:25:14 +0400

    Chance arrow function to inline callback function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
 const findTheOldest = function(arr) {  
-    return arr.reduce((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? a : b);
+    // return arr.reduce((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? a : b);
+
+    let oldest = arr.reduce(function(prev, curr) {
+        return (prev.yearOfDeath - prev.yearOfBirth) > (curr.yearOfDeath - curr.yearOfBirth) ? prev : curr;
+    });
+
+    return oldest;
+
 };
 
 // Do not edit below this line

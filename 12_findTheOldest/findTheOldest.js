635878d4c9f25ff0abde13c5f695dbc79557d6c1
Adrien <adrien.biz@gmail.com>
2022-07-12 22:30:45 +0400

    Clean up code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,12 +1,15 @@
 const findTheOldest = function(arr) {  
-    // return arr.reduce((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? a : b);
-
     let oldest = arr.reduce(function(prev, curr) {
+
+        // If person is still alive, replace year of death with current year to get age
+        if (!prev.yearOfDeath || !curr.yearOfDeath) {
+            let date =  new Date().getFullYear();
+            prev.yearOfDeath = date;
+            curr.yearOfDeath = date;
+        }
         return (prev.yearOfDeath - prev.yearOfBirth) > (curr.yearOfDeath - curr.yearOfBirth) ? prev : curr;
     });
-
     return oldest;
-
 };
 
 // Do not edit below this line

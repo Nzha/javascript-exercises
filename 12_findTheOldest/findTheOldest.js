bcef4a6893a7831df8e45d02d035c2ee7be4d996
Adrien <adrien.biz@gmail.com>
2022-07-12 22:02:30 +0400

    Function: find highest value in array of objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,5 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {  
+    return arr.reduce((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? a : b);
 };
 
 // Do not edit below this line
